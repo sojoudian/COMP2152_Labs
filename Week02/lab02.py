9f1e8e0264d19c7b266c1fe0452f03b57bff2096
sojoudian <sojodyan@gmail.com>
2026-01-23 11:31:27 -0500

    update week02

--- Week02/lab02.py
+++ Week02/lab02.py
@@ -11,9 +11,9 @@ if playerChoice < 1 or playerChoice > 3:
 else:
     computerChoice = random.randint(1, 3)
 
-    playerChoiceIndex = choices[playerChoice - 1] # ==> 0, 1, 2
-    computerChoiceIndex = choices[computerChoice - 1]
-    print(playerChoiceIndex,  computerChoiceIndex)
+#    playerChoiceIndex = choices[playerChoice - 1] # ==> 0, 1, 2
+#    computerChoiceIndex = choices[computerChoice - 1]
+#    print(playerChoiceIndex,  computerChoiceIndex)
 
     # Determine the winner logic using if/elif/else
     if playerChoice == computerChoice:
